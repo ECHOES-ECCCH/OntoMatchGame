89c361b54390558a8db0ae72dfdeb06db8db4227
clement-plancq <clement.plancq@univ-tours.fr>
2026-01-19 15:36:24 +0100

    Correct dotenv usage

--- php/connect.php
+++ php/connect.php
@@ -8,17 +8,17 @@
 	// Charge .env puis .env.local s’il existe
 	$dotenv->loadEnv(dirname(__DIR__).'/.env');
 
-	$dbHost = getenv('DB_HOST');
-	$dbHost = getenv('DB_NAME');
-	$dbHost = getenv('DB_USER');
-	$dbHost = getenv('DB_PASSWORD');
-
+	$dbHost = $_ENV['DB_HOST'];
+	$dbName = $_ENV['DB_NAME'];
+	$dbUser = $_ENV['DB_USER'];
+	$dbPassword = $_ENV['DB_PASSWORD'];
+	
 	$connection = mysqli_init();
 
 	if (!$connection) {
 		echo "false";
 	}
 	
-	mysqli_ssl_set($connection, NULL,NULL,'/shared/hncert/__db_huma-num_fr_interm_root.cer', '/dev/null', NULL);
+	#mysqli_ssl_set($connection, NULL,NULL,'/shared/hncert/__db_huma-num_fr_interm_root.cer', '/dev/null', NULL);
 	mysqli_real_connect($connection, $dbHost, $dbUser, $dbPassword, $dbName);
 ?>	
