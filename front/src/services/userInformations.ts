ecb34a76b571a3e54284539c9b01d1a964648b46
Clemence Cazin <cazin>
2026-01-29 10:13:23 +0100

    remove isLoading

--- front/src/services/userInformations.ts
+++ front/src/services/userInformations.ts
@@ -1,31 +1,20 @@
-import { ref } from 'vue'
 import api from './api'
 import { handleApiError } from './error.handler'
 
-export const isLoading = ref(false)
-
 export const getUserName = async (email: string) => {
-  isLoading.value = true
-
   try {
     const { data } = await api.get(`/getusername.php?email=${email}`)
     return data.username
   } catch (error) {
     handleApiError(error)
-  } finally {
-    isLoading.value = false
   }
 }
 
 export const getUserId = async (email: string) => {
-  isLoading.value = true
-
   try {
     const { data } = await api.get(`/getuserid.php?email=${email}`)
     return data.userId
   } catch (error) {
     handleApiError(error)
-  } finally {
-    isLoading.value = false
   }
 }
