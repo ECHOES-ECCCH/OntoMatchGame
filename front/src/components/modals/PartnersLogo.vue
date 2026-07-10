4b36564f26bb652b97b58367c71ca71a1a369995
clement-plancq <clement.plancq@univ-tours.fr>
2026-07-10 13:19:36 +0200

    Add echoes logo to credits and infos pages. Fix HN logo file name issue

--- front/src/components/modals/PartnersLogo.vue
+++ front/src/components/modals/PartnersLogo.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
+import echoes from '@/assets/img/ECHOES_logo.png'
 import takin from '@/assets/img/takin_logo.png'
-import hn from '@/assets/img/HN_LOGO.jpg'
+import hn from '@/assets/img/HN_logo.jpg'
 import indytion from '@/assets/img/indytion_logo.jpg'
 import ucMerced from '@/assets/img/uc-merced_logo.png'
 import masa from '@/assets/img/MASAplus_logo.png'
@@ -8,6 +9,7 @@ import masa from '@/assets/img/MASAplus_logo.png'
 
 <template>
   <ul class="logos">
+    <li><img alt="logo" :src="echoes" /></li>
     <li><img alt="logo" :src="takin" /></li>
     <li><img alt="logo" :src="hn" /></li>
     <li><img alt="logo" :src="indytion" /></li>
